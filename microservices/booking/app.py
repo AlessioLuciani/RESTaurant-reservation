4e3550eec53658151dc58caa88847d5e0caf8fbe
EmaMerc <mercanti.emanuele.96@gmail.com>
2021-05-18 15:25:33 +0200

    feat: add test for booking

--- microservices/booking/app.py
+++ microservices/booking/app.py
@@ -4,8 +4,8 @@ import json
 import pymongo
 import requests
 
-client = pymongo.MongoClient("localhost", 27017)
-db = client.reservations
+client = pymongo.MongoClient("mongodb://booking_db", 27017)
+db = client.restaurants
 
 app = Flask(__name__)
 api = Api(app)
@@ -26,16 +26,16 @@ class reservation(Resource):
         args = task_post_args.parse_args()
         user_session = {"authToken": args["authToken"], "email": args["email"]}
         # TODO come capisco se è un errore?
-        #is_valid = requests.get("http://user_auth_api:3000/validate", user_session)
-        #if "error" in is_valid.keys():
-        #    return "User not authorized"
+        is_valid = requests.get("http://user_auth_api:3000/validate", user_session).json()
+        if "error" in is_valid.keys():
+            return "User not authorized"
 
         # TODO controllo posti disponibili
         args.pop('authToken')
         reservation_id = db.data.insert_one(args).inserted_id
         result = {
             "body": "Reservation pending",
-            "reservation_id": str(inserted_id)
+            "reservation_id": reservation_id,
         }
         return result 
     def update(self):
@@ -47,14 +47,44 @@ class connectionCheck(Resource):
 
 class dbCheck(Resource):
     def get(self):
-        data = db.data.find_one({"nome": "test_data"})
-        data.pop("_id", None)
-        return jsonify(data)
+       return f"Avalibale databases are {list(client.list_database_names())}"
 
+
+
+class testReservation(Resource):
+    # reserve table status pending
+    def post(self):
+        # create test db
+        dblist = client.list_database_names()
+        if not "testdb" in dblist:
+            test_data = []
+            testdb = client["testdb"]
+            testcol = testdb["testcol"]
+        else:
+            testdb = client.testdb
+
+        # parse input
+        args = task_post_args.parse_args()
+        user_session = {"authToken": args["authToken"], "email": args["email"]}
+        is_valid = requests.get("http://user_auth_api:3000/validate", user_session).json()
+        if "error" in is_valid.keys():
+            return "User not authorized: "
+        # reserve places
+        args.pop('authToken')
+        reservation_id = testdb.testcol.insert_one(args).inserted_id
+        result = {
+            "body": "Reservation pending",
+            "reservation_id": reservation_id,
+        }
+        return result 
+    def update(self):
+        pass
 
 api.add_resource(connectionCheck, "/ping")
 api.add_resource(dbCheck, "/pingdb")
 api.add_resource(reservation, "/reserve")
+api.add_resource(testReservation, "/test")
+
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=False, port=3000, host='0.0.0.0') 
